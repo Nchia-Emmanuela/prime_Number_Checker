a8269a2732509c47a73c7751c7cd2fcafda2613a
Nchia-Emmanuela <83321016+Nchia-Emmanuela@users.noreply.github.com>
2021-08-21 09:39:03 +0100

    bug resolved

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def prime_checker(number):
     is_prime = True
     for i in range(2, number -1):
-        if number / i == 0:
+        if number % i == 0:
             is_prime = False
     if is_prime:
         print("It's a Prime Number")
